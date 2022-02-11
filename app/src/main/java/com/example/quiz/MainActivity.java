b01d443563cde602e639c9d9cc38982beaf2bbe5
Alan Rufi <75837895+Rufialan@users.noreply.github.com>
2022-02-11 09:43:59 +0100

    Page 1

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -8,9 +8,19 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button BT_player;
+    private Button BT_play;
+    private TextInputLayout ET_saisie_player1_layout;
+    private TextInputLayout ET_saisie_player2_layout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +29,41 @@ public class MainActivity extends AppCompatActivity {
         Toolbar mainToolBar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolBar);
 
+        BT_player = findViewById(R.id.main_player_button);
+        ET_saisie_player1_layout = findViewById(R.id.main_edit_text_player1_layout);
+        ET_saisie_player2_layout = findViewById(R.id.main_edit_text_player2_layout);
+        BT_play = findViewById(R.id.main_play_button);
 
+        ET_saisie_player1_layout.setVisibility(EditText.GONE);
+        ET_saisie_player2_layout.setVisibility(EditText.GONE);
+        BT_play.setVisibility(Button.GONE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        BT_player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ET_saisie_player1_layout.getVisibility() == View.GONE) {
+                    ET_saisie_player1_layout.setVisibility(EditText.VISIBLE);
+                    ET_saisie_player2_layout.setVisibility(EditText.VISIBLE);
+                    BT_play.setVisibility(Button.VISIBLE);
+                } else if (ET_saisie_player1_layout.getVisibility() == View.VISIBLE) {
+                    ET_saisie_player1_layout.setVisibility(EditText.GONE);
+                    ET_saisie_player2_layout.setVisibility(EditText.GONE);
+                    BT_play.setVisibility(Button.GONE);
+                    BT_play.setVisibility(Button.GONE);
+                }
+            }
+        });
+
+        BT_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
